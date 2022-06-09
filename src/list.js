568823bab514fa8556df4207c48eaf596599f7a2
Henry <dokinruss@gmail.com>
2022-06-09 12:40:24 +0000

    create the list structure of the todo list and setup enviroment for webpack

---
+++ src/list.js
@@ -0,0 +1,9 @@
+// class ListObj {
+//   constructor(description, completed, index) {
+//     this.description = description;
+//     this.completed = completed;
+//     this.index = index;
+//   }
+// }
+
+// const objArray = [];
